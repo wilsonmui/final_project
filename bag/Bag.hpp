e5098b16025687ce59dda920876edaa89a14014f
wilsonmui <wmui@umail.ucsb.edu>
2018-03-19 01:20:45 -0700

    implementations

--- bag/Bag.hpp
+++ bag/Bag.hpp
@@ -11,6 +11,7 @@ private:
 public:
     Bag(){};
     void add(T element);
+    void union(Bag bag);
     Bag* split();
     int getPennantVectorSize(){return pennantVector.size();}
 };
@@ -28,4 +29,54 @@ void Bag<T>::add(T element){
         }
     }
     pennantVector.push_back(newPennant);
+}
+
+Bag* Bag::split(){
+    Bag bag = new Bag();
+    Pennant y = pennantVector[0];
+    pennantVector[0] = NULL;
+    for(int k = 1; k < pennantVector.size(); k++){
+        if(pennantVector[k] != NULL){
+            bag->pennantVector[k-1] = pennantVector[k].split;
+            pennantVector[k-1] = pennantVector[k];
+            pennantVector[k] = NULL;
+        }
+    }
+    
+    if (y != NULL){
+        insert(y);
+    }
+    
+    return bag;
+}
+
+void Bag::union(Bag bag){
+    Pennant y = new Pennant();
+    for (int k = 0; k < pennantVector.size(); k++){
+        FA(pennantVector[k], bag->pennantVector[k], y);
+    }
+}
+
+//helper function for union. Full-adder function
+void FA(Pennant *x, Pennant *y, Pennant *z) {
+    
+    if(z == NULL && y!= NULL && x != NULL) {
+        x->merge(y);
+        z = x;
+        x = NULL;
+    }
+    else if(y == NULL && x != NULL && z != NULL) {
+        x->merge(z);
+        z = x;
+        x = NULL;
+    }
+    else if(x == NULL && y != NULL && z != NULL) {
+        y->merge(z);
+        z = y;
+        x = NULL;
+    }
+    else if (x != NULL && y != NULL && z != NULL){
+        y->merge(z);
+        z = y;
+    }
 }
